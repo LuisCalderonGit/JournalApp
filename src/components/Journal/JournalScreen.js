1ba6c1b98c53249f0a0b4b7da83761ed78bfb1e4
Luis Calderon <luis2fcalderon@gmail.com>
2022-02-07 19:13:20 -0500

    Se agrego el component sidebar y sus estilos, a su vez que el cdn de font-awesome al proyecto.

--- src/components/Journal/JournalScreen.js
+++ src/components/Journal/JournalScreen.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { AppRouter } from "../Routers/AppRouter";
+import { Sidebar } from "./Sidebar";
+
 
 export const JournalScreen = () => {
   return (
     <div className="journal__main-content">
-      <h1>JournalScreen</h1>
+
+      <Sidebar />
+
+      <main>
+        <h1>Main content</h1>
+      </main>
     </div>
   );
 };
