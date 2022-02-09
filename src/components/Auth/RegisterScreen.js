d417267bde108b18a0fc8e51848919688fe8b217
Luis Calderon <luis2fcalderon@gmail.com>
2022-02-08 21:47:00 -0500

    Se anadio la validacion basica del registro de usuario, se instalo la libreria de validaciones validator.

--- src/components/Auth/RegisterScreen.js
+++ src/components/Auth/RegisterScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import validator from "validator";
+
+
 import { useForm } from "../../hooks/useForm";
 
 export const RegisterScreen = () => {
@@ -8,25 +11,43 @@ export const RegisterScreen = () => {
   const [formValues, handleInputChange] = useForm({
     name: 'Luis Calderon',
     email: 'luis@gmail.com',
-    password: '1234',
-    confirmPassword: '1234'
+    password: '12345',
+    confirmPassword: '12345'
   });
 
   const { name, email, password, confirmPassword } = formValues;
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(name, email, password, confirmPassword)
+    if (isOkForm()) {
+      console.log('Formulario correcto');
+    }
   }
 
   const isOkForm = () => {
+    if (name.trim().length === 0) {
+      console.log('name is required');
+      return false;
+    } else if (!validator.isEmail(email)) {
+      console.log('email is not validate');
+      return false;
+    } else if (password !== confirmPassword || password.length < 5) {
+      console.log("password short");
+      return false;
+    }
 
+    return true;
   }
   return (
     <>
       <h3 className="auth__title">Register</h3>
 
       <form onSubmit={handleRegister}>
+
+        <div className="auth__alert-error">
+          Hola
+        </div>
+
         <input
           value={name}
           onChange={handleInputChange}
