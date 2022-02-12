a3f4fe8d1425d3e6ea1ceb0e2ff28089de2deeae
Luis Calderon <luis2fcalderon@gmail.com>
2022-02-11 21:24:00 -0500

    Actualizacion de funciones para que se muestren las notas traidas de a base de datos, reestructuracion del hook useForm para que funcione de manera correcta con las notas

--- src/components/Journal/JournalEntry.js
+++ src/components/Journal/JournalEntry.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
-export const JournalEntry = ({ id, tittle, date, body, url }) => {
-    // console.log(id, tittle, date, body, url);
-    return <div className='journal__entry pointer'>
+const advancedFormat = require("dayjs/plugin/advancedFormat");
+dayjs.extend(advancedFormat);
+
+export const JournalEntry = ({ id, title, date, body, url }) => {
+    const day = dayjs(date);
+
+    const dispatch = useDispatch();
+
+
+    const handleClickNote = () => {
+        dispatch(activeNote(id, { title, date, body, url }));
+
+    }
+
+    return <div className='journal__entry pointer' onClick={handleClickNote}>
         {
             url &&
             <div className='journal__entry-picture' style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url()`,
+                backgroundImage: `url(${url})`,
             }}></div>
         }
 
         <div className='journal__entry-body'>
 
-            <p className='journal__entry-title'>{tittle}</p>
+            <p className='journal__entry-title'>{title}</p>
             <p className='journal__entry-content'>
                 {body}
             </p>
         </div>
         <div className='journal__entry-date-box'>
-            <span>Monday</span>
-            <h4>24</h4>
+            <span>{day.format('dddd')}</span>
+            <h4>{day.format('Do')}</h4>
         </div>
     </div>;
 };
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+
 import { NotesAppBar } from './NotesAppBar';
 
+
 export const NoteScreen = () => {
+
+    const { active: noteActive } = useSelector(state => state.notes);
+
+    const [formValues, handleInputChange, reset] = useForm(noteActive);
+    const { body, url } = formValues;
+
+
+    const activeId = useRef(noteActive.id);
+    useEffect(() => {
+        if (noteActive.id !== activeId.current) {
+            reset(noteActive);
+            activeId.current = noteActive.id;
+        }
+
+    }, [noteActive, reset])
+
+
+
+
+
     return <div className='notes__main-content'>
+
         <NotesAppBar />
         <div className='notes__content'>
             <input type="text"
                 placeholder='some awesome title'
                 className='notes__title-input'
+                value={noteActive.title}
+                onChange={handleInputChange}
             />
             <textarea placeholder='what happened today'
-                className='notes__textarea'></textarea>
-            <div className='notes__image'>
-                <img
-                    src='https://tierragamer.com/wp-content/uploads/2021/08/luffy-b-450x300.jpg'
-                    alt='luffy'
-                />
+                className='notes__textarea'
+                value={body}
+                onChange={handleInputChange}
+            ></textarea>
 
-            </div>
+            {
+                url &&
+                (<div className='notes__image'>
+                    <img
+                        src={`${url}`}
+                    />
+
+                </div>)
+            }
         </div>
     </div>;
 }; 
